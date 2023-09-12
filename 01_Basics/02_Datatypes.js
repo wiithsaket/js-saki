7156c83fd0f32b93f7a8e09564d52d245587d03b
Saket <140685368+wiithsaket@users.noreply.github.com>
2023-09-12 12:16:13 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+//alert //we are using node js, not browser
+
+console.log(3
+    +
+4
+)//code readability should be high
+
+let name = "Saket"
+let age = 14
+
+//number
+//Bigint
+//string=> ""
+//boolean=> true/false
+//null=> standalone value/representation of empty value
+//undefined =>
+//symbol=> unique
+
+console.log(typeof null);//object
+console.log( typeof undefined);//undefined
+
+//objects
